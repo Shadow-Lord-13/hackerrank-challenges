789afac4109c76231f41f978d6baa55d6b7d496a
Tushar Singh Bisht <tusharbisht2011@gmail.com>
2024-10-06 17:51:33 +0530

    Adding comments

--- Easy/Palindrome split/main.py
+++ Easy/Palindrome split/main.py
@@ -1,7 +1,8 @@
-# method to calculate the max disjoint palindromic subsequences that a string can have 
+# method to calculate the max disjoint palindromic subsequences that a string can have.
 def max_palindrome_seq(s):
     frequency = {}
 
+#loop to calculate total no. value for each chat and store it in the frequecy dir.
     for char in s:
         if char in frequency:
             frequency[char] += 1
@@ -10,6 +11,7 @@ def max_palindrome_seq(s):
 
     count_palin = 0
 
+#count the palindrone with min len 2 example: if we have 3 a the it will form 1 disjoint palindrome sequence "aa" leaving 1 a or you can say ("aaa") since there no other char for single a to from palindrome with min len 2.If we have 4 a then it will form 2 disjoint palindromice seq "aa" "aa".
     for feq in frequency.values():
         count_palin += feq//2
 
